8843f27258ac74b5c5a579a8a3ef0e41c246771a
Dragomir-Ficard Iulia-Estera <dfiestera@gmail.com>
2021-10-28 21:19:56 +0300

    added Person and User classes

---
+++ src/Classes/Person.java
@@ -0,0 +1,45 @@
+package Classes;
+
+public class Person {
+    private String _firstName;
+    private String _lastName;
+    private String _id;
+
+    public Person(){}
+    public Person(String firstName, String lastName, String id){
+        _firstName = firstName;
+        _lastName = lastName;
+        _id = id;
+    }
+
+    public String get_firstName() {
+        return _firstName;
+    }
+
+    public void set_firstName(String _firstName) {
+        this._firstName = _firstName;
+    }
+
+    public String get_lastName() {
+        return _lastName;
+    }
+
+    public void set_lastName(String _lastName) {
+        this._lastName = _lastName;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String toString(){
+        String s = "Nume: "+_lastName+
+                "\nPrenume: "+_firstName+
+                "\nID: "+ _id;
+        return s;
+    }
+}
