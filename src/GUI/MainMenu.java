3817b5f6cd341626e60bb4d4646812570878c34b
trakker01 <gabormaxim02@gmail.com>
2021-10-29 17:39:44 +0300

    Updated the first page

--- src/GUI/MainMenu.java
+++ src/GUI/MainMenu.java
@@ -1,8 +1,31 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu {
     private JButton Admin;
     private JButton User;
+    private JPanel MainPanel;
+    private JFrame MainMenu;
+
+    public MainMenu(){
+        this.MainMenu = new JFrame("Main Menu");
+
+        Admin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public void displayMainMenu(){
+        MainMenu.setContentPane(this.MainPanel);
+        MainMenu.setSize(1920, 1080);
+        MainMenu.setVisible(true);
+    }
+
+    public JPanel getMainPanel(){return MainPanel;}
 }
