8f8f8fbe36931b0fe0d8171e4c5dccbbe6faa0bc
trakker01 <gabormaxim02@gmail.com>
2021-10-31 19:26:45 +0200

    Update

---
+++ src/GUI/AdminCamere.java
@@ -0,0 +1,27 @@
+package GUI;
+
+import javax.swing.*;
+
+public class AdminCamere {
+    private JButton camera123CheckInButton;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JList Sortare;
+    private JButton button15;
+
+    public AdminCamere(){
+        
+    }
+}
