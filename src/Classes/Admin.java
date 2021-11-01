8b23e4e10bed54fc54397cac06486e84229e955c
Dragomir- Ficard Iulia- Estera <31253349+DragomirIuliaEstera@users.noreply.github.com>
2021-11-01 13:21:20 +0200

    Create Admin class

---
+++ src/Classes/Admin.java
@@ -0,0 +1,32 @@
+package Classes;
+
+public class Admin {
+    private static Admin _instance = new Admin();
+    private Room[] _rooms;
+    private int _roomsNumber;
+
+    private Admin(){
+
+    }
+    private Admin(String camp){
+
+    }
+    public static Admin getInstance(){
+        if(null==_instance){
+            _instance = new Admin();
+        }
+        return _instance;
+    }
+
+    public void setRooms(Room[] r, int roomsNumber){
+        _rooms = r;
+        _roomsNumber = roomsNumber;
+    }
+
+    public String afisareCamere(){
+        String s = "";
+        for(int i = 0; i<_roomsNumber; ++i)
+            s= s+" Numar camera : " + i+" "+ _rooms[i].toString()+"\n";
+        return s;
+    }
+}
