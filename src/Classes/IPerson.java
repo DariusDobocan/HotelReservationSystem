3b3e85db0e1cdaa0f81f619be259a35c4a41daad
Dragomir- Ficard Iulia- Estera <31253349+DragomirIuliaEstera@users.noreply.github.com>
2021-11-02 16:04:12 +0200

    Create IPerson.java

---
+++ src/Classes/IPerson.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public interface IPerson {
+    public String get_firstName();
+    public void set_firstName(String _firstName);
+    public String get_lastName();
+    public void set_lastName(String _lastName);
+    public String get_id();
+    public void set_id(String _id);
+    public String toString();
+}
